85634402f49748c9381cd51071eaa8e360722745
shreyaskambla <kshreyas20@gmail.com>
2019-01-18 00:18:30 -0500

    added print statements

--- Test1/src/program2.java
+++ Test1/src/program2.java
@@ -14,6 +14,7 @@ public class program2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		System.out.println("asd");
 	}
 
 }
